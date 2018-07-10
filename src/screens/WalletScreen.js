4286757a4ceefeb4e923df6d43570db14d5682f7
Richard Burton <burtonic@gmail.com>
2018-07-10 16:02:37 -0400

    Alpha Screen First Pass
    Errors when you try to run it.

--- src/screens/WalletScreen.js
+++ src/screens/WalletScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, Button, View, Text, Image } from 'react-native';
 import Card from '../components/Card';
 import CoinRow from '../components/CoinRow';
 import Container from '../components/Container';
@@ -8,6 +8,28 @@ import Section from '../components/Section';
 import WalletMenu from '../components/WalletMenu';
 import { apiGetAccountBalances } from '../helpers/api';
 import * as ethWallet from '../model/ethWallet';
+
+class ModalScreen extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'flex-start',
+          justifyContent: 'center',
+          padding: 20,
+          backgroundColor: '#111111',
+        }}
+      >
+        <Text style={{ fontSize: 20, color: 'white', paddingBottom: 10 }}>Welcome to Balance</Text>
+        <Image style={{ width: 16, height: 16 }} source={require('../assets/exclamation-point-circle.png')} />
+        <Text style={{ fontSize: 15, color: '#FFAF24', paddingBottom: 10 }}>This is alpha software.</Text>
+        <Text style={{ fontSize: 15, color: 'white', width: '75%' }}>Please do not store more in your wallet than you are willing to lose.</Text>
+        <Button onPress={() => this.props.navigation.goBack()} title="Create a Wallet" />
+      </View>
+    );
+  }
+}
 
 class WalletScreen extends Component {
   state = {
@@ -61,7 +83,7 @@ class WalletScreen extends Component {
           {this.state.wallet &&
             this.state.wallet.assets.map(asset => {
               const coinLogo =
-                asset.symbol === 'ETH'
+                asset.symbol === 'ETHA'
                   ? 'https://raw.githubusercontent.com/balance-io/tokens/master/images/ethereum_1.png'
                   : `https://raw.githubusercontent.com/balance-io/tokens/master/images/${asset.address}.png`;
               return <CoinRow key={asset.symbol} imgPath={coinLogo} coinSymbol={asset.symbol} coinName={asset.name} coinBalance={asset.balance} />;
@@ -80,4 +102,4 @@ class WalletScreen extends Component {
   }
 }
 
-export default WalletScreen;
+export default ModalScreen;
